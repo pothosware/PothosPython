001991852f1c26d39604e90c71718cc9afb1635a
Josh Blum <josh@joshknows.com>
2016-09-30 15:13:38 -0700

    functioning python loader

--- PythonConfLoader.cpp
+++ PythonConfLoader.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2016-2016 Josh Blum
 // SPDX-License-Identifier: BSL-1.0
 
+#include <Pothos/Util/BlockDescription.hpp>
 #include <Pothos/Plugin.hpp>
 #include <Pothos/System.hpp>
 #include <Pothos/Proxy.hpp>
@@ -15,13 +16,21 @@
  * and invokes it with the specified arguments.
  **********************************************************************/
 static Pothos::Object opaquePythonLoaderFactory(
+    const Poco::Path &rootPath,
+    const std::string &targetName,
     const std::string &moduleName,
-    const std::string &className,
+    const std::string &functionName,
     const Pothos::Object *args,
     const size_t numArgs)
 {
     //create python environment
     auto env = Pothos::ProxyEnvironment::make("python");
+
+    //add to the system path
+    //TODO only add if not already added...
+    auto sys = env->findProxy("sys");
+    auto sysPath = sys.callProxy("get:path");
+    sysPath.callProxy("append", rootPath.toString());
 
     //convert arguments into proxy environment
     std::vector<Pothos::Proxy> proxyArgs(numArgs);
@@ -34,7 +43,7 @@ static Pothos::Object opaquePythonLoaderFactory(
     auto mod = env->findProxy(moduleName);
 
     //call into the factory
-    auto block = mod.getHandle()->call(className, proxyArgs.data(), proxyArgs.size());
+    auto block = mod.getHandle()->call(functionName, proxyArgs.data(), proxyArgs.size());
     return Pothos::Object(block);
 }
 
@@ -59,6 +68,18 @@ static std::vector<Pothos::PluginPath> PythonLoader(const std::map<std::string, 
         throw Pothos::Exception("missing confFileSection");
     const auto &target = confFileSectionIt->second;
 
+    //extract module name
+    const auto moduleIt = config.find("module");
+    if (moduleIt == config.end() or moduleIt->second.empty())
+        throw Pothos::Exception("missing module");
+    const auto &module = moduleIt->second;
+
+    //extract function name (class path or function name)
+    const auto functionIt = config.find("function");
+    if (functionIt == config.end() or functionIt->second.empty())
+        throw Pothos::Exception("missing function");
+    const auto &function = functionIt->second;
+
     //doc sources: scan sources unless doc sources are specified
     std::vector<std::string> docSources;
     const auto docSourcesIt = config.find("doc_sources");
@@ -75,7 +96,32 @@ static std::vector<Pothos::PluginPath> PythonLoader(const std::map<std::string, 
     if (factoriesIt != config.end()) for (const auto &factory :
         Poco::StringTokenizer(factoriesIt->second, ",", tokOptions))
     {
-        factories.push_back(Pothos::PluginPath("/blocks").join(factory.substr(1)));
+        factories.push_back(Pothos::PluginPath("/blocks", factory));
+    }
+
+    //generate JSON block descriptions
+    Pothos::Util::BlockDescriptionParser parser;
+    for (const auto &source : docSources) parser.feedFilePath(source);
+
+    //store block paths in handle, and store doc paths
+    for (const auto &factory : parser.listFactories())
+    {
+        const auto pluginPath = Pothos::PluginPath("/blocks/docs", factory);
+        Pothos::PluginRegistry::add(pluginPath, parser.getJSONObject(factory));
+        entries.push_back(pluginPath);
+        factories.push_back(Pothos::PluginPath("/blocks", factory));
+    }
+
+    //register for all factory paths
+    for (const auto &pluginPath : factories)
+    {
+        const auto factory = Pothos::Callable(&opaquePythonLoaderFactory)
+            .bind(rootDir, 0)
+            .bind(target, 1)
+            .bind(module, 2)
+            .bind(function, 3);
+        Pothos::PluginRegistry::addCall(pluginPath, factory);
+        entries.push_back(pluginPath);
     }
 
     return entries;
