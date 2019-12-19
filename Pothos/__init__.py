e1c078bd66c79cfdedeeef9c0cdf70091903d68b
Nicholas Corgan <n.corgan@gmail.com>
2019-12-19 16:50:43 -0600

    All warnings set through Python's warnings module go through Poco::Logger("py.warnings")

--- Pothos/__init__.py
+++ Pothos/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2014-2016 Josh Blum
+#                    2019 Nicholas Corgan
 # SPDX-License-Identifier: BSL-1.0
 
 from . PothosModule import *
@@ -10,3 +11,12 @@ from . Topology import Topology
 from . BlockRegistry import BlockRegistry
 from . Logger import LogHandler
 from . Packet import Packet
+
+import logging
+
+# logging.captureWarnings() redirects all outputs from the "warnings" module
+# to a Python logger named "py.warnings". Adding our log handler to this logger
+# results in all Python warnings being consumed by our infrastructure.
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("py.warnings").addHandler(LogHandler("py.warnings"))
+logging.captureWarnings(True)
